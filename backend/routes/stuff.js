d25e36cb3d41b12fe1d940f8f4e7f49ee40d639f
SvoronaM <sergeymigal@gmail.com>
2022-10-26 20:33:27 +0200

    stuff.js

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/api/stuff', (req, res, next) => {
+router.post('/', (req, res, next) => {
     delete req.body._id;
     const thing = new Thing({
         ...req.body
@@ -11,25 +11,25 @@ router.post('/api/stuff', (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
 });
 
-router.put('/api/stuff/:id', (req, res, next) => {
+router.put('/:id', (req, res, next) => {
     Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet modifié !'}))
         .catch(error => res.status(400).json({ error }));
 });
 
-router.delete('/api/stuff/:id', (req, res, next) => {
+router.delete('/:id', (req, res, next) => {
     Thing.deleteOne({ _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
         .catch(error => res.status(400).json({ error }));
 });
 
-router.get('/api/stuff/:id', (req, res, next) => {
+router.get('/:id', (req, res, next) => {
     Thing.findOne({ _id: req.params.id })
         .then(thing => res.status(200).json(thing))
         .catch(error => res.status(404).json({ error }));
 });
 
-router.get('/api/stuff', (req, res, next) => {
+router.get('/', (req, res, next) => {
     Thing.find()
         .then(things => res.status(200).json(things))
         .catch(error => res.status(400).json({ error }));
