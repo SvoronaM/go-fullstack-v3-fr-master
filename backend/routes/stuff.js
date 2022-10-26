6710e84ca49bb46b17ec22004d9d04b6576be973
SvoronaM <sergeymigal@gmail.com>
2022-10-26 21:20:58 +0200

    stuff.js

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -4,25 +4,9 @@ const router = express.Router();
 const stuffCtrl = require('../controllers/stuff');
 
 router.post('/', stuffCtrl.createThing);
-
-router.put('/:id', );
-
-router.delete('/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-        .catch(error => res.status(400).json({ error }));
-});
-
-router.get('/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json(thing))
-        .catch(error => res.status(404).json({ error }));
-});
-
-router.get('/', (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json(things))
-        .catch(error => res.status(400).json({ error }));
-});
+router.put('/:id', stuffCtrl.modifyThing);
+router.delete('/:id', stuffCtrl.deleteThing);
+router.get('/:id', stuffCtrl.getOneThing);
+router.get('/', stuffCtrl.getAllThing);
 
 module.exports = router;
