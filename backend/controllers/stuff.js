9d9be23268199e0640ee56cf9dc03c3e5b63e7b4
SvoronaM <sergeymigal@gmail.com>
2022-10-26 21:06:22 +0200

    stuff.js

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,1 +1,16 @@
-const Thing = require('../models/thing');
+const Thing = require('../models/Thing');
+
+exports.createThing = (req, res, next) => {
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body
+    });
+    thing.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+        .catch(error => res.status(400).json({ error }));
+}
+exports.modifyThing = (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+        .catch(error => res.status(400).json({ error }));
+}
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,23 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const Thing = require('../models/thing');
+const stuffCtrl = require('../controllers/stuff');
 
-router.post('/', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-        .catch(error => res.status(400).json({ error }));
-});
+router.post('/', stuffCtrl.createThing);
 
-router.put('/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-        .catch(error => res.status(400).json({ error }));
-});
+router.put('/:id', );
 
 router.delete('/:id', (req, res, next) => {
     Thing.deleteOne({ _id: req.params.id })
